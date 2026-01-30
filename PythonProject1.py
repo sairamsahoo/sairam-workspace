df7e437c68d14d1a0a94c15788f30276bee834d5
Sairam Sahoo <sairamsahoo2000@gmail.com>
2026-01-30 22:25:14 +0530

    Project -1 Sum of the N Number

--- PythonProject1.py
+++ PythonProject1.py
@@ -1,1 +1,5 @@
-print("Hello Sairam World")
+#print("Hello Sairam World")
+# Project:-1 Sum of the Number 
+n=int(input("Enter a Number : "))
+sum_number= (n*(n+1)/2)
+print(f'The Sum of {n} Number is {sum_number}')
